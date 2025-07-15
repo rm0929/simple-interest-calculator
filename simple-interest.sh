b2b8acd90fdce1d340154d5e0a9c1addb81cf178
Rohan Mathur <rohanmathur2001@gmail.com>
2025-07-15 11:07:22 -0400

    simple-interest.sh file created

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter Principal:"
+read principal
+echo "Enter Rate of Interest:"
+read rate
+echo "Enter Time (in years):"
+read time
+
+interest=$(echo "$principal * $rate * $time / 100" | bc)
+echo "Simple Interest = $interest"
